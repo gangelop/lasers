3a9ecdc47a7769f1e382c545968f6f24d6394ca1
nmarjano <nmarjano@redhat.com>
2018-06-18 15:59:21 +0200

    Do you smell that?

--- laser.sh
+++ laser.sh
@@ -2,9 +2,13 @@
 
 # usage: just run the thing.
 echo 'Firing lasers...'
+sleep 2
+echo 'In the name of our supreme leader and Lord!    Pepe Le ...'
+sleep 2
 for i in $(seq 10); do
     echo 'PEW!'
     sleep 0.5
 done
+sleep 1
 
 exit 0
